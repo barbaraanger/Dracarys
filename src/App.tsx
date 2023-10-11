c3c0885e0dbbf0c5aa28d832daa3c9d44bbdd9bb
barbaraanger <barbaraanger@onepeloton.com>
2023-10-11 11:35:07 -0300

    creating components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import React from "react";
 import "./App.css";
-import { AppBar, ListItem, Toolbar, Typography } from "@mui/material";
 import { appStyles } from "./styles";
-import { fetchAllCharacters } from "./client/fetchCharacters";
 
 function App() {
   return (
@@ -12,11 +12,7 @@ function App() {
         </Toolbar>
       </AppBar>
       <div>
-        <List>
-          {fetchAllCharacters.forEach((element) => {
-            <ListItem>{element}</ListItem>
-          })}
-        </List>
+        <CharactersList />
       </div>
     </div>
   );
--- src/client/fetchCharacters.ts
+++ src/client/fetchCharacters.ts
@@ -1,4 +1,10 @@
 import axios from "axios";
 import { envVariables } from "../environment";
 
-export const fetchAllCharacters = axios.get(`${envVariables.url}`);
+export const fetchAllCharacters = axios
+  .get(`${envVariables.url}`)
+  .then((response) => response)
+  .catch((error) => {
+    console.log(error);
+    return error;
+  });
