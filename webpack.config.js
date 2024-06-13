f056bb20ac384463fc0e9f3c184f6128884f3ee4
Bárbara Anger <barbaraanger@hotmail.com>
2024-06-13 16:15:15 -0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 // webpack.config.js
+const path = require("path");
+const crypto = require("crypto");
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
 
 module.exports = {
-    // ... other Webpack configuration ...
-  
+    entry: "./src/",
     resolve: {
       fallback: {
         path: require.resolve("path-browserify"),
